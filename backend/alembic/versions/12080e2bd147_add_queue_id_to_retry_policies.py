95496daf31da78c3d965e9f53b98a38ebc64e645
Sricharan1105 <ss8968@srmist.edu.in>
2026-07-05 18:38:01 +0530

    Fix retry policy migration chain

--- backend/alembic/versions/12080e2bd147_add_queue_id_to_retry_policies.py
+++ backend/alembic/versions/12080e2bd147_add_queue_id_to_retry_policies.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 revision: str = "12080e2bd147"
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = "61ecc33f4ff2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
